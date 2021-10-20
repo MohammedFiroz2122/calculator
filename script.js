c7495062bb539d3c4b17b2fc54cc957a6e676dd6
MohammedFiroz2122 <firozbadar11@gmail.com>
2021-10-20 09:51:50 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,5 @@
+
+var displayValue=(num)=>result.value+=num
+var clearScreen=()=>result.value=""
+var evaluateExpression=()=>result.value=eval(result.value)
+var backSpace=()=>result.value=Math.floor(result.value/10)
